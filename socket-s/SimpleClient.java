d485c694e0db58fa20c6e34654d1e1dfe3bf93f9
r3yz0n <reyzon.thapa2020@gmail.com>
2024-07-29 08:47:14 +0545

    feat: Refactor socket communication code and add ClientSocketDemo and ServetSocketDemo classes

---
+++ socket-s/SimpleClient.java
@@ -0,0 +1,16 @@
+
+// package socket;
+
+// Simple Client Example
+import java.net.*;
+import java.io.*;
+
+public class SimpleClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 8080); // Connect to server on localhost, port 8080
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String messageFromServer = in.readLine();
+        System.out.println("Server says: " + messageFromServer);
+        socket.close();
+    }
+}
---
+++ socket-s/SimpleServer.java
@@ -0,0 +1,15 @@
+
+// Simple Server Example
+import java.net.*;
+import java.io.*;
+
+public class SimpleServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8080); // Server will listen on port 8080
+        System.out.println("Waiting for client...");
+        Socket clientSocket = serverSocket.accept(); // This will wait until a client connects
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        out.println("Hello, client!");
+        serverSocket.close();
+    }
+}
